ee6537c19d5fba6df2cdfa56757cf6db96618a09
EilonV <73593619+EilonV@users.noreply.github.com>
2024-05-09 15:56:22 +0300

    ga4

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,12 +1,11 @@
-import { useEffect } from "react";
 import { Home } from "./pages/home";
 import ReactGA from "react-ga4";
 
 function App() {
   ReactGA.initialize('G-KNYSCDC71F')
-  useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-  }, []);
+
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 
   return (
     <Home />
