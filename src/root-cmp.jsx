87afa8e39f063626455e106f36c707082498401c
EilonV <73593619+EilonV@users.noreply.github.com>
2024-05-09 14:15:45 +0300

    ga4 page views tracker

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import { Home } from "./pages/home";
 import ReactGA from "react-ga4";
 
 function App() {
   ReactGA.initialize('G-KNYSCDC71F')
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
 
   return (
     <Home />
